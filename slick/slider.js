9ce44a6774701d1287d563887d850c2181637cc4
Ilya Saulin <aragvaa@gmail.com>
2020-05-13 21:55:47 +0300

    init comit 1

---
+++ slick/slider.js
@@ -0,0 +1,11 @@
+$(".regular").slick({
+    // для точек снизу
+    dots: true,
+    infinite: true,
+    // slidesToShow: 3,
+    // slidesToScroll: 3 
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // Для авто перемотки
+    autoplay: true,
+});
